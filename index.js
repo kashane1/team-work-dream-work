acb2c8e0636b1fc0db1f9f9d15d6285e46ab1823
Kashane S <ksakhakorn@gmail.com>
2022-03-07 21:26:15 -0800

    made some progress with all the constructors and using inquirer to build each of the employees. still a long ways to go

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const Employee = require("./Employee");
-
-
-
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const Employee = require("./lib/Employee");
 
 // need to create a new list of Employees
 const employeeArray = new Employee;
